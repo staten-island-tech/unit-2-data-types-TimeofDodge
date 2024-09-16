a8856e02a065fc07d85a2ee79cfbea340a34a932
TimeofDodge <gabrielvishnevetskiy@gmail.com>
2024-09-16 09:20:05 -0400

    day 1

---
+++ tipcalc.py
@@ -0,0 +1,14 @@
+bill = 0
+tip = 0
+tap = 0
+
+bill = float(input('skibslice'))
+tip = int(input('fanum'))
+#tap = bill+tip
+
+#print(tap)
+
+values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i)
